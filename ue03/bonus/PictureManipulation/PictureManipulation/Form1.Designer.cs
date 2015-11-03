86289c55f893f726d4575337ccb70ec95e50c087
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2015-11-04 00:20:42 +0100

    final commit assignment3

---
+++ ue03/bonus/PictureManipulation/PictureManipulation/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace PictureManipulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "invert",
+            "clamp (75,180)",
+            "multiply by 4 and clamp (2,253)",
+            "uniform quantization to 16 values (integer divide by 16, multy by 16)",
+            "threshold with 128",
+            "Take the blue channel ",
+            "convert grayscale",
+            "swap colors to GRB",
+            "5x5 Box filter",
+            "Sobel x ",
+            "Sobel y"});
+            this.comboBox1.Location = new System.Drawing.Point(9, 411);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(426, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "invert";
+            // 
+            // button3
+            // 
+            this.button3.Image = global::PictureManipulation.Properties.Resources.exec;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(175, 441);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 34);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Ausführen";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::PictureManipulation.Properties.Resources.save1;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(272, 441);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 34);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Speichern";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::PictureManipulation.Properties.Resources.folder_violet_open1;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(78, 441);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 34);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Öffnen";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(426, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "jpg files (*.jpg)|*.jpg";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 487);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "PictureManipulator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ue03/bsp12/prog.cs
@@ -0,0 +1,125 @@
+
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace asd
+{
+    class fgh
+    {
+        static void Main(string[] args)
+        {
+        	
+            Image i = Image.FromFile(args[1]);
+			Bitmap img = new Bitmap(i);
+
+			switch(args[0]){
+                case "1": blur(img);
+                    break;
+                case "2": sobelx(img);
+                    break;
+                case "3": sobely(img);
+                    break;
+            }
+			
+			
+        }
+
+		static void sobelx(Bitmap img){
+			Bitmap newimg = new Bitmap(img.Width, img.Height);
+         	int r;
+        	Color c;
+			int[,] sobelmatrix = {
+				{(-1),0,1},
+				{(-2),0,2},
+				{(-1),0,1}};
+			for(int x = 1; x < img.Width-1; x++){
+				for(int y = 1; y < img.Height-1; y++){
+					r = 0;
+					for(int i = -1; i < 2; i++){
+						for(int j = -1; j < 2; j++){
+							r += (img.GetPixel(x+i,y+j).R * sobelmatrix[i+1,j+1]);
+						}
+					}
+					r = (int)Math.Sqrt(r*r);
+					if(r > 230) r = 255;
+					if(r < 50) r = 0;
+					c = Color.FromArgb(r,r,r);
+					newimg.SetPixel(x,y,c);
+				}
+			}
+			newimg.Save("sobelx.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+		}
+
+		static void sobely(Bitmap img){
+			Bitmap newimg = new Bitmap(img.Width, img.Height);
+         	int r;
+        	Color c;
+			int[,] sobelmatrix = {
+				{(-1),(-2),(-1)},
+				{0,0,0},
+				{1,2,1}};
+			for(int x = 1; x < img.Width-1; x++){
+				for(int y = 1; y < img.Height-1; y++){
+					r = 0;
+					for(int i = -1; i < 2; i++){
+						for(int j = -1; j < 2; j++){
+							r += (img.GetPixel(x+i,y+j).R * sobelmatrix[i+1,j+1]);
+						}
+					}
+					r = (int)Math.Sqrt(r*r);
+					if(r > 230) r = 255;
+					if(r < 50) r = 0;
+					c = Color.FromArgb(r,r,r);
+					newimg.SetPixel(x,y,c);
+				}
+			}
+			newimg.Save("sobely.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+		}
+
+        static void blur(Bitmap img){
+         	
+         	Bitmap newimg = new Bitmap(img.Width, img.Height);
+         	int r,g,b,innerx, innerX, innery, innerY;
+        	Color c;
+
+        	for(int x = 0; x < img.Width; x++){
+				for(int y = 0; y < img.Height ; y++){
+					innerx = x-3;
+					innerX = x+3;
+					innery = y-3;
+					innerY = y+3;
+					if(x < 3){
+						innerx = 0;
+					}
+					else if(x > img.Width-3){
+						innerX = img.Width;
+					}
+					if(y < 3){
+						innery = 0;
+					}
+					else if(y > img.Height-3){
+						innerY = img.Height;
+					}
+					r = 0;
+					g = 0;
+					b = 0;
+					for(int i = innerx; i < innerX; i++){
+						for(int j = innery; j < innerY; j++){
+							r += img.GetPixel(i,j).R;
+							g += img.GetPixel(i,j).G;
+							b += img.GetPixel(i,j).B;
+						}
+					}
+					c = Color.FromArgb(r/49,g/49,b/49);
+					newimg.SetPixel(x,y,c);
+
+				}
+			}
+			newimg.Save("blur.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+    	}
+    }
+}
